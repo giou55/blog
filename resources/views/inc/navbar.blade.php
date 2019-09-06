bdd1aca786e1aedfcc843d76d9db876515b8ad7c
Giorgos Giourmetakis <giorgosggg@gmail.com>
2019-09-06 15:39:47 +0300

    changes

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -3,10 +3,16 @@
         <div class="navbar-header">
             <a class="navbar-brand" href="/">{{config('app.name', 'MyBlog')}}</a>
         </div>
-        <ul class="nav navbar-nav">
-            <li class="active"><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/services">Services</a></li>
-        </ul>
+        <div id="navbar">
+            <ul class="nav navbar-nav">
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/services">Services</a></li>
+                <li><a href="/posts">Blog</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="/posts/create">Create Post</a></li>
+            </ul>
+        </div>
     </div>
 </nav>
