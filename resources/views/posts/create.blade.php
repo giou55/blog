2c107ef7447d531aa2c66b563f266c7888d1f7f0
Giorgos Giourmetakis <giorgosggg@gmail.com>
2019-09-06 16:07:03 +0300

    changes

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,9 +8,9 @@
             {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
         </div>
         <div class="form-group">
-                {{Form::label('body', 'Body')}}
-                {{Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body Text'])}}
-            </div>
+            {{Form::label('body', 'Body')}}
+            {{Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body Text'])}}
+        </div>
             {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
     {!! Form::close() !!}
 
