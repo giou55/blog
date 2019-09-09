aade584b89a3b0ae0d57dbd7d2e32438174024f9
Giorgos Giourmetakis <giorgosggg@gmail.com>
2019-09-09 15:45:46 +0300

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,28 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <title>{{config('app.name', 'MyBlog')}}</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </head>
-    <body>
-        
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
         @include('inc.navbar')
         <div class="container">
             @include('inc.messages')
             @yield('content')
         </div>
+    </div>
 
-        <script src="//cdn.ckeditor.com/4.4.7/standard/ckeditor.js"></script>
-        <script>
-            CKEDITOR.replace( 'article-ckeditor' );
-        </script>
-
-    </body>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/services', 'PagesController@services');
 Route::resource('posts', 'PostsController');
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
